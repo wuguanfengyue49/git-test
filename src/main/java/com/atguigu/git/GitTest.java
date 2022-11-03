f0ff0bf67211399f642d0bc03c19d245fff07456
Layne <Layne@atguigu.com>
2022-11-03 14:21:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Date:2022/11/3  14:17
+ * Author:yhy
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
